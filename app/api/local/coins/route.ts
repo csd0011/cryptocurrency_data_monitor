ece400f92613f68f28cd92775475b31fe20a1f7e
JC <jared@JCs-MacBook-Air.local>
2026-01-17 01:29:55 -0600

    modify dashboard api request

--- app/api/local/coins/route.ts
+++ app/api/local/coins/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&price_change_percentage=24h')
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=2&page=1&price_change_percentage=24h')
   const data = await res.json()
   return NextResponse.json(data)
 }
